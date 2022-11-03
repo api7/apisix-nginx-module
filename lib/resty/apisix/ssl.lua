832981e8e7d01ff7e10b7a58d98327ad54b79ec8
spacewander <spacewanderlzx@gmail.com>
2022-11-03 11:09:37 +0800

    feat: add GM C API set cert/priv_key
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

---
+++ lib/resty/apisix/ssl.lua
@@ -0,0 +1,65 @@
+local ffi = require("ffi")
+local base = require("resty.core.base")
+local errmsg = base.get_errmsg_ptr()
+local get_request = base.get_request
+local FFI_OK = base.FFI_OK
+local C = ffi.C
+local ffi_str = ffi.string
+
+
+base.allows_subsystem("http")
+
+
+ffi.cdef[[
+    typedef intptr_t        ngx_flag_t;
+    int ngx_http_apisix_set_gm_cert(void *r, void *cdata, char **err, ngx_flag_t type);
+    int ngx_http_apisix_set_gm_priv_key(void *r, void *cdata, char **err, ngx_flag_t type);
+]]
+
+
+local NGX_HTTP_APISIX_SSL_ENC = 1
+local NGX_HTTP_APISIX_SSL_SIGN = 2
+local _M = {}
+
+
+function _M.set_gm_cert(enc_cert, sign_cert)
+    local r = get_request()
+    if not r then
+        error("no request found")
+    end
+
+    local rc = C.ngx_http_apisix_set_gm_cert(r, enc_cert, errmsg, NGX_HTTP_APISIX_SSL_ENC)
+    if rc ~= FFI_OK then
+        return nil, ffi_str(errmsg[0])
+    end
+
+    local rc = C.ngx_http_apisix_set_gm_cert(r, sign_cert, errmsg, NGX_HTTP_APISIX_SSL_SIGN)
+    if rc ~= FFI_OK then
+        return nil, ffi_str(errmsg[0])
+    end
+
+    return true
+end
+
+
+function _M.set_gm_priv_key(enc_pkey, sign_pkey)
+    local r = get_request()
+    if not r then
+        error("no request found")
+    end
+
+    local rc = C.ngx_http_apisix_set_gm_priv_key(r, enc_pkey, errmsg, NGX_HTTP_APISIX_SSL_ENC)
+    if rc ~= FFI_OK then
+        return nil, ffi_str(errmsg[0])
+    end
+
+    local rc = C.ngx_http_apisix_set_gm_priv_key(r, sign_pkey, errmsg, NGX_HTTP_APISIX_SSL_SIGN)
+    if rc ~= FFI_OK then
+        return nil, ffi_str(errmsg[0])
+    end
+
+    return true
+end
+
+
+return _M
